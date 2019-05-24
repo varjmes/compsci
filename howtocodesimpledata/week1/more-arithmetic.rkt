04d6e6c968a536038f0964aa6f28b622f4c31f98
James Spencer <hello@jmes.tech>
2019-05-24 16:05:06 +0100

    feat: more arithmetic

---
+++ howtocodesimpledata/week1/more-arithmetic.rkt
@@ -0,0 +1,11 @@
+#!/usr/binx/racket
+#lang htdp/bsl
+
+; PROBLEM: 
+; 
+; Write the BSL expression that represents the arithmetic expression:     
+; (6 + 3) * (9 - 7)
+; which equals 18
+
+(* (+ 6 3)
+   (- 9 7))
