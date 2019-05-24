fd818aad35aebfc38b377e181f74a5ebaff2c244
James Spencer <hello@jmes.tech>
2019-05-24 16:11:26 +0100

    feat: even more arithmetic

---
+++ howtocodesimpledata/week1/even-more-arithmetic.rkt
@@ -0,0 +1,8 @@
+#!/usr/binx/racket
+#lang htdp/bsl
+ 
+; PROBLEM:
+; Write the BSL expression that represents the arithmetic expression (7 - 2) * 4 which equals 20. 
+; Do not just write 20! Instead write BSL expression that clearly mirrors  (7 - 2) * 4.
+
+(* (- 7 2) 4)
